b3e125efcb49709d055204cf057ef2520a840c56
Igor Berezin <igor.berezin@js-dev.pro>
2026-03-19 20:02:09 +0200

    fixed assests in the vue module, added override for the bg transparency

--- src/CV.vue
+++ src/CV.vue
@@ -5,6 +5,7 @@
 <script>
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import htmlContent from '!!raw-loader!../index.html';
+import photoUrl from '../assets/photo.jpg';
 
 export default {
   name: 'CvPage',
@@ -15,11 +16,25 @@ export default {
   },
   created() {
     const doc = new DOMParser().parseFromString(htmlContent, 'text/html');
+
+    // Replace photo src with webpack-resolved URL
+    const imgs = doc.querySelectorAll('img[src*="photo"]');
+    imgs.forEach(function (img) {
+      img.setAttribute('src', photoUrl);
+    });
+
     this.html = doc.body.innerHTML;
   },
 };
 </script>
 
 <style>
+@import '../assets/fontawesome.min.css';
+@import '../assets/fa-solid.min.css';
+@import '../assets/fa-brands.min.css';
 @import '../assets/style.css';
+
+.underlay {
+  background-color: transparent;
+}
 </style>
